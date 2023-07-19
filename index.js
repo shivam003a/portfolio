8a4cb8e582787fb408b66f9fd7f1690ac67a7eec
Shivam Singh <shivam003a@gmail.com>
2023-07-20 01:16:42 +0530

    added aboutme, project section

--- index.js
+++
@@ -1,8 +0,0 @@
-let boyImage = document.querySelector('.boy');
-
-document.addEventListener('scroll', (e)=>{
-    let x = boyImage.getBoundingClientRect().y;
-    console.log(boyImage.getBoundingClientRect());
-
-    boyImage.setAttribute("transform" , `translateX(0.2*${x})`);
-})
