b3e6fdcd6cecb0bc8311761a0017feafbb79ee9f
Shivam Singh <shivam003a@gmail.com>
2023-07-20 20:22:47 +0530

    added footer and responsiveness

---
+++ index.js
@@ -0,0 +1,18 @@
+// let slides = document.querySelectorAll('.slide');
+// let prev = document.querySelector('.prev');
+// let next = document.querySelector('.next');
+
+// slides.forEach((slide,index)=>{
+//     slide.style.left = `${index*100}%`;
+//     console.log(slide)
+//     console.log(index)
+// })
+
+// setInterval(()=>{
+//     slides.forEach((slide, index)=>{
+//         let left = parseInt(slide.style.left.replace("%",""));
+
+//         slide.style.transform = `translateX(${left - 100}%)`;
+//         console.log(left)
+//     })
+// },3000)
